a210652a36090b0ad410cda5482328fd56698ecb
Pedro Morosini <phmorosini@hotmail.com>
2024-10-03 16:57:49 -0300

    Feature incluindo a configuração dinamica dos diretorios foi finalizada.

---
+++ ProjetoCriadorDePasta/Classes/DiretoriosConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCriadorDePasta.Classes
+{
+    public class DiretoriosConfig
+    {
+        public string Diretorio1 { get; set; }
+        public string Diretorio2 { get; set; }
+    }
+}
